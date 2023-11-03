fbf4cc75e1552babd25b674086d871039afff1e3
sharper <hi@sammyharper.com>
2023-11-03 14:31:43 -0500

    feat: add the ability to pass mite name as a command line argument

--- mites.py
+++ mites.py
@@ -1,5 +1,6 @@
 import asana
 import json
+import sys
 
 # define text colors (to show success/fail messages)
 class colors:
@@ -17,24 +18,33 @@ client = asana.Client.access_token(tokens["accessToken"])
 # remove pesky deprecation warnings
 client.LOG_ASANA_CHANGE_WARNINGS = False
 
-loopVal = True
-while loopVal == True:
+# function for accepting user input
+def getUserInput():
   # get user input (what to name mite)
-  miteVal = input(colors.reset + "What is this mite? (X/C to Exit): ")
+  userInput = input(colors.reset + "What is this mite? (X/C to Exit): ")
 
-  if miteVal.lower() == 'exit' or miteVal.lower() == 'x' or miteVal.lower() == 'c':
+  if userInput.lower() == 'exit' or userInput.lower() == 'x' or userInput.lower() == 'c':
     tokensFile.close()
     print(colors.reset)
-    loopVal = False
   else:
-    try:
-      # send POST request
-      result = client.tasks.create_task({"name": miteVal, "assignee": tokens["assigneeGid"], "workspace": tokens["workspaceGid"], "projects": [tokens["projectGid"]], "tags": [tokens["tagsGid"]]}, opt_pretty=True)
-      # success message
-      print(colors.green + "beep-boop: new mite made! You can access it here: " + result["permalink_url"])
+    createMite(userInput)
 
-    except KeyError:
-      print(colors.red + "Something doesn't look right with your token configuration. Please reference the README to confirm it is set up correctly and try again.")
-    except:
-      print(colors.red + "It looks like something happened with your request. Ensure the right values are placed in your token configuration and try again.")
-      
+# function for actually making request
+def createMite(miteVal):
+  try:
+    # send POST request
+    result = client.tasks.create_task({"name": miteVal, "assignee": tokens["assigneeGid"], "workspace": tokens["workspaceGid"], "projects": [tokens["projectGid"]], "tags": [tokens["tagsGid"]]}, opt_pretty=True)
+    # success message
+    print(colors.green + "beep-boop: new mite made! You can access it here: " + result["permalink_url"])
+
+  except KeyError:
+    print(colors.red + "Something doesn't look right with your token configuration. Please reference the README to confirm it is set up correctly and try again.")
+  except:
+    print(colors.red + "It looks like something happened with your request. Ensure the right values are placed in your token configuration and try again.")
+  getUserInput()
+
+# PROGRAM START
+if len(sys.argv) >= 2:
+  createMite(sys.argv[1])
+else:
+  getUserInput()
