3d7fc832f044ac4745369235cbc944a990679025
sharper <hi@sammyharper.com>
2023-11-03 12:56:17 -0500

    feat: include colored text to highlight success/warning messages

--- mites.py
+++ mites.py
@@ -1,6 +1,12 @@
 import asana
 import json
 
+# define text colors (to show success/fail messages)
+class colors:
+  green = "\033[1;32m"
+  red = "\033[1;32m"
+  reset = "\033[0m"
+
 # read file contents of necessary tokens/gids
 tokensFile = open("tokens/tokens.json", "r")
 # convert JSON to dict
@@ -14,13 +20,13 @@ client.LOG_ASANA_CHANGE_WARNINGS = False
 loopVal = True
 while loopVal == True:
   # get user input (what to name mite)
-  miteVal = input("\033[0m What is this mite? (X/C to Exit): ")
+  miteVal = input(colors.reset + "What is this mite? (X/C to Exit): ")
 
   if miteVal.lower() == 'exit' or miteVal.lower() == 'x' or miteVal.lower() == 'c':
     tokensFile.close()
-    print("\033[0m")
+    print(colors.reset)
     loopVal = False
   else:
     # send POST request
     result = client.tasks.create_task({"name": miteVal, "assignee": tokens["assigneeGid"], "workspace": tokens["workspaceGid"], "projects": [tokens["projectGid"]], "tags": [tokens["tagsGid"]]}, opt_pretty=True)
-    print("\033[1;32m beep-boop: new mite made! You can access it here: " + result["permalink_url"])
+    print(colors.green + "beep-boop: new mite made! You can access it here: " + result["permalink_url"])
