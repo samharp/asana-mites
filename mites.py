26fb9f5bcd727ca71e0043c6906ac11fa74a1187
sharper <hi@sammyharper.com>
2023-11-28 19:26:06 -0600

    fix: catch additional errors when getting user input

--- mites.py
+++ mites.py
@@ -22,33 +22,43 @@ client = asana.Client.access_token(tokens["accessToken"])
 client.LOG_ASANA_CHANGE_WARNINGS = False
 
 def welcomeUser():
-  print(colors.reset + "----------")
-  print("Welcome to Mitebox Manager!")
-  getUserInput()
+  try:
+    print(colors.reset + "----------")
+    print("Welcome to Mitebox Manager!")
+    getUserInput()
+  except KeyboardInterrupt:
+    sys.exit()
 
 # function for accepting user input
 def getUserInput():
-  # get user input (what to name mite)
-  print(colors.reset + "__________")
-  print("Enter the name of your new mite or one of the following commands:")
-  print(colors.cyan + "C" + colors.reset + " to Clean: move done items to Done Pile")
-  print(colors.cyan + "I" + colors.reset + " for Info: fetch basic stats on your Mitebox)")
-  print(colors.cyan + "Z" + colors.reset + " for Random: fetch a random mite to work on")
-  print(colors.cyan + "X" + colors.reset + " to Exit: terminate Mitebox Manager")
-  userInput = input(colors.cyan + ": ")
+  try:
+    # get user input (what to name mite)
+    print(colors.reset + "__________")
+    print("Enter the name of your new mite or one of the following commands:")
+    print(colors.cyan + "C" + colors.reset + " to Clean: move done items to Done Pile")
+    print(colors.cyan + "I" + colors.reset + " for Info: fetch basic stats on your Mitebox)")
+    print(colors.cyan + "Z" + colors.reset + " for Random: fetch a random mite to work on")
+    print(colors.cyan + "X" + colors.reset + " to Exit: terminate Mitebox Manager")
+    userInput = input(colors.cyan + ": ")
 
-  if userInput.lower() == "exit" or userInput.lower() == "x":
-    tokensFile.close()
-    # print(colors.reset)
+    if userInput.lower() == "exit" or userInput.lower() == "x":
+      tokensFile.close()
+      print(colors.reset + "xxxxxxxxxx")
+      # sys.exit()
+    elif userInput.lower() == "clean" or userInput.lower() == "c":
+      cleanMitebox()
+    elif userInput.lower() == "info" or userInput.lower() == "i":
+      getMiteboxInfo()
+    elif userInput.lower() == "rando" or userInput.lower() == "z":
+      getRandoMite()
+    else:
+      createMite(userInput)
+  except KeyboardInterrupt:
     print(colors.reset + "xxxxxxxxxx")
-  elif userInput.lower() == "clean" or userInput.lower() == "c":
-    cleanMitebox()
-  elif userInput.lower() == "info" or userInput.lower() == "i":
-    getMiteboxInfo()
-  elif userInput.lower() == "rando" or userInput.lower() == "z":
-    getRandoMite()
-  else:
-    createMite(userInput)
+    sys.exit()
+  except:
+    print(colors.red + "It looks like something happened when getting user input. Please follow the dialogs and try again.")
+    print(colors.reset)
 
 # function for creating task (sending request)
 def createMite(miteVal):
@@ -68,8 +78,14 @@ def createMite(miteVal):
 
   except KeyError:
     print(colors.red + "something doesn't look right with your token configuration. please reference the README to confirm it is set up correctly and try again.")
+    print(colors.reset)
+  except KeyboardInterrupt:
+    print(colors.reset + "xxxxxxxxxx")
+    sys.exit()
   except:
     print(colors.red + "it looks like something happened with your request. ensure the right values are placed in your token configuration and try again.")
+    print(colors.reset)
+
   getUserInput()
 
 # function for cleaning up mitebox
@@ -94,48 +110,64 @@ def cleanMitebox():
       print(colors.green + "beep-boop: your Mitebox is all cleaned up! " + str(numberOfTasks) + " completed task(s) moved to the Done Pile.")
     else:
       print("looks like there were no completed tasks to be moved to the Done Pile...")
+  except KeyboardInterrupt:
+    print(colors.reset + "xxxxxxxxxx")
+    sys.exit()
   except:
     print(colors.red + "it looks like something happened when trying to get completed tasks in your Mitebox. check your setup and try again.")
+    print(colors.reset)
   getUserInput()
 
 # function for getting info on Mitebox
 def getMiteboxInfo():
   x = 0
-  result = client.tasks.get_tasks({"section": tokens["doPileGid"], "opt_fields": ["completed"]})
+  try:
+    result = client.tasks.get_tasks({"section": tokens["doPileGid"], "opt_fields": ["completed"]})
 
-  resultList = list(result)
+    resultList = list(result)
 
-  for i in resultList:
-    # if it's been completed
-    if(i["completed"] == True):
-      x += 1
+    for i in resultList:
+      # if it's been completed
+      if(i["completed"] == True):
+        x += 1
 
-  print(colors.green + "there are currently " + str(len(resultList)) + " Mites in your Do Pile")
+    print(colors.green + "there are currently " + str(len(resultList)) + " Mites in your Do Pile")
 
-  if x > 0:
-    print(str(x) + " of them are/is completed. clean your Mitebox to move these to the Done Pile.")
-
+    if x > 0:
+      print(str(x) + " of them are/is completed. clean your Mitebox to move these to the Done Pile.")
+  except KeyboardInterrupt:
+    print(colors.reset + "xxxxxxxxxx")
+    sys.exit()
+  except:
+    print(colors.red + "It looks like something happened when trying to get info on your Mitebox. Check your setup and try again.")
+    print(colors.reset)
   getUserInput()
 
 def getRandoMite():
   x = 0
+  try:
+    # when it hasn't found a completed task
+    while x < 1:
+      # get all tasks in to-do section
+      result = client.tasks.get_tasks({"section": tokens["doPileGid"], "opt_fields": ["completed", "name", "permalink_url"]})
+      # get random item 
+      randoMite = random.choice(list(result))
 
-# when it hasn't found a completed task
-  while x < 1:
-    # get all tasks in to-do section
-    result = client.tasks.get_tasks({"section": tokens["doPileGid"], "opt_fields": ["completed", "name", "permalink_url"]})
-    # get random item 
-    randoMite = random.choice(list(result))
+      # if it hasnt been completed...
+      if(randoMite["completed"] == False):
+        # print("we found one!")
+        print(colors.green + "hey! what about this: ")
+        print(randoMite["name"] + "; you can access it here: " + randoMite["permalink_url"])
+        x += 1
+      else:
+        print(colors.red + "i can pick one faster if you clean out your Mitebox first...")
 
-    # if it hasnt been completed...
-    if(randoMite["completed"] == False):
-      # print("we found one!")
-      print(colors.green + "hey! what about this: ")
-      print(randoMite["name"] + "; you can access it here: " + randoMite["permalink_url"])
-      x += 1
-    else:
-      print(colors.red + "i can pick one faster if you clean out your Mitebox first...")
-
+  except KeyboardInterrupt:
+    print(colors.reset + "xxxxxxxxxx")
+    sys.exit()
+  except:
+    print(colors.red + "It looks like something happened when trying to get a random mite. Check your setup and try again.")
+    print(colors.reset)
   getUserInput()
 
 # PROGRAM START
